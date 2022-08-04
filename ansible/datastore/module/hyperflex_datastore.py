548c992d914f7b3c5839f045390b277781c2f079
oldguardmd <oldguard@msn.com>
2022-08-04 09:18:56 -0400

    Corrected bug with Verify

--- ansible/datastore/module/hyperflex_datastore.py
+++ ansible/datastore/module/hyperflex_datastore.py
@@ -246,7 +246,7 @@ class hyperFlex():
         # 2) Check for existing datastore of the same name
         hx_url = f"https://{required_fields['hx_connect_ip']}/rest/datastores"
         returned_data, info = self.getData(hx_url=hx_url, data=None, method='GET')
-        if (re.match(r'2..',str(info['status'])) and returned_data is not None):
+        if (re.match(r'2..',str(info['status']))):
             self.module.fail_json(msg=f"We failed to connect to the API as expected.{info}")
         #for item in returned_data:
         for item in json.loads(returned_data.read()):
